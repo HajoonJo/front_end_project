c3e57dd65ca29cc27fcf856e153a69419199415b
HAJOON <jojoshua380@gmail.com>
2025-06-18 15:29:34 +0000

    update all project files

--- react-with-flask/vite.config.js
+++ react-with-flask/vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3000,
-    host: '0.0.0.0'
-  }
-}
+    port: 5173,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
